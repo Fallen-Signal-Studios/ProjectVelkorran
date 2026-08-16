6858a4e20a842dfb57efca569d761049615534a9
fallensignalstudios <fallensignalstudios@gmail.com>
2026-08-15 19:24:01 -0700

    Add Narrative and GAS module dependencies

--- Source/ProjectVelkorran/ProjectVelkorran.Build.cs
+++ Source/ProjectVelkorran/ProjectVelkorran.Build.cs
@@ -7,17 +7,21 @@ public class ProjectVelkorran : ModuleRules
 	public ProjectVelkorran(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"EnhancedInput",
+				"GameplayAbilities",
+				"GameplayTags",
+				"GameplayTasks",
+				"NarrativeArsenal",
+				"NarrativeSaveSystem"
+			}
+		);
 	}
 }
